30bf6b69032164efe895f52111308e86abace9fb
Raj Sharma <rajsharma331.rajvnsjob@gmail.com>
2026-07-30 22:44:54 +0530

    connect to lie backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Timeline from "./components/Timeline";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Timeline from "./components/Timeline";
+
 function App() {
+  useEffect(() => {
+    fetch("https://portfolio-backend-sayi.onrender.com", {
+      method: "POST",
+    }).catch(console.error);
+  }, []);
+
   return (
     <div className="bg-slate-950 text-gray-100 min-h-screen">
       <Navbar />
